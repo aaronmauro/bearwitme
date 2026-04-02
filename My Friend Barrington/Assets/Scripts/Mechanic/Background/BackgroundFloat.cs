02d635bd856b60d8b4f26ff98f280d1b45ed332b
MightyGoose0 <ryanbokla@gmail.com>
2026-04-02 15:54:25 -0400

    Random bg item, Ship animation trigger extended and fixed seeing under level

--- My Friend Barrington/Assets/Scripts/Mechanic/Background/BackgroundFloat.cs
+++ My Friend Barrington/Assets/Scripts/Mechanic/Background/BackgroundFloat.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class BackgroundFloat : MonoBehaviour
 {
-    [SerializeField]
-    public float floatHeight = 0.5f;   // How high it moves
-    [SerializeField]
-    public float floatSpeed = 1f;      // How fast it moves
+    [SerializeField] private float minHeight = 0.6f;
+    [SerializeField] private float maxHeight = 1f;
+
+    [SerializeField] private float minSpeed = 0.75f;
+    [SerializeField] private float maxSpeed = 2f;
 
     private Vector3 startPos;
+    private float floatHeight;
+    private float floatSpeed;
+    private float randomOffset;
 
     void Start()
     {
         startPos = transform.position;
+
+        floatHeight = Random.Range(minHeight, maxHeight);
+        floatSpeed = Random.Range(minSpeed, maxSpeed);
+        randomOffset = Random.Range(0f, Mathf.PI * 2f);
     }
 
     void Update()
     {
-        float newY = Mathf.Sin(Time.time * floatSpeed) * floatHeight;
-        transform.position = startPos + new Vector3(0, newY, 0);
+        float y = Mathf.Sin(Time.time * floatSpeed + randomOffset) * floatHeight;
+        transform.position = startPos + Vector3.up * y;
     }
 }
